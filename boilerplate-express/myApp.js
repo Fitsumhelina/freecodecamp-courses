073b416b2e0ae83872ad915f44d3c014f328335a
Fitsumhelina <permin1501424@gmail.com>
2024-08-03 00:46:19 +0300

    100% done

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
+const bodyParser = require('body-parser');
 
 // Load environment variables from a .env file if it exists
 dotenv.config();
 
+
+// Middleware to handle URL-encoded data
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Example for serving static assets from the "public" directory
 app.use("/public", express.static(__dirname + "/public"));
@@ -46,8 +50,53 @@ app.get("/json", (req, res) => {
 });
 
 
+// Middleware to add current time to the request object
+const addTimeMiddleware =(req,res,next) => {
+    req.time = new Date().toString();
+    next();
+};
+
+// method 1
+// Final handler to respond with the time
+const timeHandler =(req,res,) => {
+    res.json({time: req.time})
+};
+app.get('/now', addTimeMiddleware, timeHandler);
 
 
+// metjod 2
+// Route definition for /now with middleware and final handler
+app.get("/now",addTimeMiddleware, (req,res) => {
+    res.json({time: req.time})
+})
+
+//// Echo server route handler
+app.get("/:word/echo",(req,res) => {
+    const word = req.params.word;
+    res.json({echo: word})
+})
+
+
+// Route handler for /name
+app.get ("/name", (req,res) => {
+
+    const firstName= req.query.first
+    const lastName= req.query.last
+    
+    res.json({ name:`${firstName} ${lastName}` });
+
+})
+
+
+//  Get Data from POST Requests
+app.post ("/name", (req,res) => {
+
+    const firstName= req.body.first
+    const lastName= req.body.last
+    
+    res.json({ name:`${firstName} ${lastName}` });
+
+})
 
 
 
