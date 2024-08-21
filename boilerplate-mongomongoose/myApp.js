507f59d858e94cc7b033e708843ece8306578980
Fitsumhelina <permin1501424@gmail.com>
2024-08-22 02:50:54 +0300

    58% complated on 8/22/2024

--- boilerplate-mongomongoose/myApp.js
+++ boilerplate-mongomongoose/myApp.js
@@ -71,9 +71,10 @@ const findOneByFood = (food, done) => {
   });
 };
 
-const findPersonById = (personId, done) => {
-  done(null /*, data*/);
-};
+Person.findById(personId, function (err, data) {
+  if (err) return console.log(err);
+  done(null, data);
+});
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
