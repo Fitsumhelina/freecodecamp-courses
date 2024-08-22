b8970d1a1a479cc066bc9d60e66807f051a38709
Fitsumhelina <permin1501424@gmail.com>
2024-08-22 03:17:55 +0300

    67% complated on 8/22/2024

--- boilerplate-mongomongoose/myApp.js
+++ boilerplate-mongomongoose/myApp.js
@@ -77,7 +77,23 @@ Person.findById(personId, function (err, data) {
 });
 
 const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
+  const foodToAdd = 'hamburger';
+  
+  // .findById() method to find a person by _id with the parameter personId as search key. 
+  Person.findById(personId, (err, person) => {
+    if(err) return console.log(err); 
+  
+    // Array.push() method to add "hamburger" to the list of the person's favoriteFoods
+    person.favoriteFoods.push(foodToAdd);
+
+    // and inside the find callback - save() the updated Person.
+    person.save((err, updatedPerson) => {
+      if(err) return console.log(err);
+      done(null, updatedPerson)
+    })
+  })
+};
+
 
   done(null /*, data*/);
 };
